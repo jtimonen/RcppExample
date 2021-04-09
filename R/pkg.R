118564abb0366abc5f5b993964195db445a80488
Juho Timonen <juho.timonen@aalto.fi>
2021-04-09 23:05:12 +0300

    Initial commit

---
+++ R/pkg.R
@@ -0,0 +1,9 @@
+#' Example package using Rcpp
+#' 
+#' @docType package
+#' @name RcppExample
+#' @useDynLib RcppExample, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @author Juho Timonen
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(RcppExample)
+
+test_check("RcppExample")
